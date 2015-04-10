5df05ecbd75cf432774e790206f76cd63c905f0d
Sujay Mansingh <sujaymansingh@gmail.com>
2015-04-10 16:47:33 +0100

    [#1] Add helper command to list requirements
    This way we can run:
    python setup.py requirements | xargs pip install

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import setuptools
+import sys
 
 
 REQUIREMENTS = [
@@ -7,6 +8,11 @@ REQUIREMENTS = [
 
 
 if __name__ == "__main__":
+    if len(sys.argv) > 1 and sys.argv[1] == "requirements":
+        for req in REQUIREMENTS:
+            print req
+        sys.exit(0)
+
     setuptools.setup(
         name="runners",
         version="0.0.1",
