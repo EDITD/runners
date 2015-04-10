058370934d1f926101395b7f7ae83a35b594793f
Sujay Mansingh <sujaymansingh@gmail.com>
2015-04-10 14:06:16 +0100

    Add initial structure

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+
+REQUIREMENTS = [
+]
+
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name="runners",
+        version="0.0.1",
+        author="EDITD",
+        author_email="engineering@editd.com",
+        packages=setuptools.find_packages(),
+        scripts=[],
+        url="https://github.com/EDITD/runners",
+        license="LICENSE.txt",
+        description="Help with running stuff",
+        long_description="View the github page (https://github.com/EDITD/runners) for more details.",
+        install_requires=REQUIREMENTS
+    )
