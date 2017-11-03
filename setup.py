337a3c6c13dd3f987cd72f13768edb8b8d884799
Grant Stephens <grant@stephens.co.za>
2017-11-03 13:15:12 +0000

    Added get_version to setup.py to read the version from the changelog

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 
+import re
 import sys
 
 import setuptools
@@ -7,6 +8,18 @@ import setuptools
 REQUIREMENTS = [
     "kazoo==2.0",
 ]
+
+# Regex matching pattern for v followed by 2 or 3 numerical values ending with
+# a newline
+pattern = re.compile('([v]\d+\.)+(\d+\.){0,1}(\d)(?:$|\n)')
+
+
+def get_version():
+    with open('CHANGELOG.md', 'r') as fn:
+        while True:
+            version = pattern.findall(fn.readline())
+            if len(version) > 0:
+                return ''.join(version[0])[1:]
 
 
 if __name__ == "__main__":
@@ -17,7 +30,7 @@ if __name__ == "__main__":
 
     setuptools.setup(
         name="runners",
-        version="0.0.5",
+        version=get_version(),
         author="EDITD",
         author_email="engineering@editd.com",
         packages=setuptools.find_packages(),
@@ -25,6 +38,7 @@ if __name__ == "__main__":
         url="https://github.com/EDITD/runners",
         license="LICENSE.txt",
         description="Help with running stuff",
-        long_description="View the github page (https://github.com/EDITD/runners) for more details.",
+        long_description="View the github page " +
+            "(https://github.com/EDITD/runners) for more details.",
         install_requires=REQUIREMENTS
     )
